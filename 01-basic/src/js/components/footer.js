838ec767b9e9321e6e56e65bf64a74c604ee791e
puroc <p00u00r00>
2018-02-07 15:24:53 +0800

    1

--- 01-basic/src/js/components/footer.js
+++ 01-basic/src/js/components/footer.js
@@ -1,8 +1,12 @@
 import React from 'react';
+
+var footerCss  =  require("../../css/footer.css");
+
 export default class ComponentFooter extends React.Component{
+
   render(){
     return (
-      <footer>
+      <footer class={footerCss.miniFooter}>
         <h1>这里是页脚</h1>
       </footer>
     )
--- 01-basic/src/js/components/header.js
+++ 01-basic/src/js/components/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 export default class ComponentHeader extends React.Component{
   render(){
     return (
-      <header>
+      <header className="smallFontSize">
         <h1>这里是页头</h1>
       </header>
     )
