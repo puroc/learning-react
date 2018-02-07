1aebe4a0fe8a53388e0d8e340b6a289b4bfb3cf4
puroc <p00u00r00>
2018-02-07 16:47:45 +0800

    1

--- 01-basic/src/js/components/bodyindex.js
+++ 01-basic/src/js/components/bodyindex.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BodyChild from './bodychild';
+import { Input } from 'antd';
 
 export default class BodyIndex extends React.Component {
 	constructor() {
@@ -29,6 +30,7 @@ export default class BodyIndex extends React.Component {
 		return (
 			<div>
 				<h2>页面的主体内容</h2>
+				<Input placeholder="Basic usage" />
 				<input id="submitButton" ref="submitButton" type="button" value="提交" onClick={this.handleClick.bind(this, "pud",27)}/>
 				<p>从首页面获得的参数[props] id:{this.props.id},userid:{this.props.userid},username:{this.props.username}</p>
 				<p>本组件的状态值[state] username:{this.state.username},age:{this.state.age}</p>
--- 01-basic/webpack.config.js
+++ 01-basic/webpack.config.js
@@ -14,11 +14,14 @@ module.exports = {
         presets: ['react', 'es2015'],
         plugins: ['react-html-attrs'] //添加组件的插件配置
       }
-    },//下面是添加的 css 的 loader，也即是 css 模块化的配置方法，大家可以拷贝过去直接使用
-    {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
-    }
+    },
+    //下面是使用 ant-design 的配置文件
+    { test: /\.css$/, loader: 'style-loader!css-loader' },
+    // //下面是添加的 css 的 loader，也即是 css 模块化的配置方法，大家可以拷贝过去直接使用
+    // {
+    //   test: /\.css$/,
+    //   loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+    // }
     ]
   },    
   output: {
