6d3f0e60d8662c92b00c0c02cf3b32f870767df3
puroc <p00u00r00>
2018-02-09 11:40:35 +0800

    1

--- 02-project/src/js/components/pc_index.js
+++ 02-project/src/js/components/pc_index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PCHeader from './pc_header.js';
-
+import PCFooter from './pc_footer.js';
 export default class PCIndex extends React.Component {
   render() {
     return (
       <div>
         <PCHeader/>
+        <PCFooter/>
       </div>
       );
   }
